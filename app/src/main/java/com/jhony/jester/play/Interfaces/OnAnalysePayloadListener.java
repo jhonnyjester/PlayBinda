9be0765330831f9f6993fdfff7bb6d142598a327
Jhony <rodriguesjnyadr@gmail.com>
2018-01-29 23:16:46 +0530

    Added back interface: OnAnalysePayload

---
+++ app/src/main/java/com/jhony/jester/play/Interfaces/OnAnalysePayloadListener.java
@@ -0,0 +1,12 @@
+package com.jhony.jester.play.Interfaces;
+
+import com.jhony.jester.play.Model.GsonModel;
+
+/**
+ * Created by JAR on 29-01-2018.
+ */
+
+public interface OnAnalysePayloadListener {
+
+    public void OnAnalysePayload(GsonModel gsonModel);
+}
